d739237e90ab0bdbee48bba2b8f133cd5014be9d
啓大 齋藤 <240004@jc-21.jp>
2025-10-20 00:39:49 +0900

    マクロを使用しない形の安全なポインタ解放系関数を追加

--- Engine/Global.h
+++ Engine/Global.h
@@ -1,7 +1,41 @@
 #pragma once
 #include "Direct3D.h"
+#include <concepts>
 
 //安全にメモリを開放するためのマクロ
 #define SAFE_DELETE(p) {if ((p)!=nullptr) { delete (p); (p)=nullptr;}}
 #define SAFE_DELETE_ARRAY(p) {if ((p)!=nullptr) { delete[] (p); (p)=nullptr;}}
 #define SAFE_RELEASE(p) {if ((p)!=nullptr) { p->Release(); (p)=nullptr;}}
+
+template<typename TYPE>
+concept HasRelease = requires(TYPE* _ptr)
+{
+	{ _ptr->Release() } -> std::same_as<void>;
+};
+
+template<typename TYPE>
+static inline void SafeDelete(TYPE* _ptr)
+{
+	if (_ptr != nullptr)
+	{
+		delete _ptr;
+		_ptr = nullptr;
+	}
+}
+
+template<typename TYPE>
+static inline void SafeRelease(TYPE* _ptr)
+{
+	if (HasRelease<TYPE>)
+	{
+		if (_ptr != nullptr)
+		{
+			_ptr->Release();
+			_ptr = nullptr;
+		}
+	}
+	else
+	{
+		SafeDelete(_ptr);
+	}
+}
