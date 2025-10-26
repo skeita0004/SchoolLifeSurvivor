4b80e5bad787984588664fef6369a0df335486cc
啓大 齋藤 <240004@jc-21.jp>
2025-10-27 01:07:29 +0900

    ステージ追加

---
+++ Stage.cpp
@@ -0,0 +1,74 @@
+﻿#include "Stage.h"
+#include <vector>
+#include "CsvReader.h"
+#include "Player.h"
+
+namespace
+{
+    // 複数ステージを用意（やらないけど）するならメンバ変数にすること。
+    std::vector<std::vector<int>> stageMap(0, std::vector<int>(0));
+}
+
+Stage::Stage(GameObject* _parent):
+    GameObject(_parent, "Stage")
+{
+}
+
+Stage::~Stage()
+{
+}
+
+void Stage::Initialize()
+{
+    Instantiate<Player>(GetParent());
+    CsvReader stageData;
+    //stageData.Load("map/map01.csv");
+
+    for (int y = 0; y < stageData.GetHeight(); y++)
+    {
+        for (int x = 0; x < stageData.GetWidth(); x++)
+        {
+            BlockType block = static_cast<BlockType>(stageData.GetIntValue(x, y));
+            
+            switch (block) 
+            {
+            case Stage::BlockType::B_GRASS:
+                break;
+            case Stage::BlockType::B_BRICK:
+                break;
+            case Stage::BlockType::B_CLOUD_L:
+                break;
+            case Stage::BlockType::B_CLOUD_MID:
+                break;
+            case Stage::BlockType::B_CLOUD_R:
+                break;
+            case Stage::BlockType::E_GAME:
+                break;
+            case Stage::BlockType::E_PHONE:
+                break;
+            case Stage::BlockType::I_BED:
+                break;
+            case Stage::BlockType::I_CLASS:
+                break;
+            case Stage::BlockType::I_HOMEWORK:
+                break;
+            case Stage::BlockType::I_COMPUTER:
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
+
+void Stage::Update()
+{
+}
+
+void Stage::Draw()
+{
+}
+
+void Stage::Release()
+{
+}
---
+++ Stage.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "GameObject.h"
+
+class Stage : public GameObject
+{
+public:
+    Stage(GameObject* _parent);
+    ~Stage();
+
+    void Initialize()    override;
+    void Update()  override;
+    void Draw()    override;
+    void Release() override;
+
+private:
+    enum struct BlockType : int
+    {
+        B_GRASS = 0,
+        B_BRICK,
+        B_CLOUD_L,
+        B_CLOUD_MID,
+        B_CLOUD_R,
+
+        E_GAME = 10,
+        E_PHONE,
+
+        I_BED = 20,
+        I_CLASS,
+        I_HOMEWORK,
+        I_COMPUTER
+    };
+};
