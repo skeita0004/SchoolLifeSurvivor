ce21598b8a632b238b140267260a731117d278b8
啓大 齋藤 <240004@jc-21.jp>
2025-10-27 01:07:39 +0900

    プレイヤー追加

---
+++ Player.cpp
@@ -0,0 +1,39 @@
+﻿#include "Player.h"
+#include "Input.h"
+
+Player::Player(GameObject* _parent) :
+    GameObject(_parent, "Player"),
+    hModel_(-1)
+{
+}
+
+Player::~Player()
+{
+}
+
+void Player::Initialize()
+{
+    hModel_ = Model::Load("player.fbx");
+}
+
+void Player::Update()
+{
+    if (Input::IsKey(DIK_A))
+    {
+        transform_.position_.x -= 2.f;
+    }
+    if (Input::IsKey(DIK_D))
+    {
+        transform_.position_.x += 2.f;
+    }
+}
+
+void Player::Draw()
+{
+    Model::SetTransform(hModel_, transform_);
+    Model::Draw(hModel_);
+}
+
+void Player::Release()
+{
+}
---
+++ Player.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "Engine/GameObject.h"
+#include "Model.h"
+
+class Player : public GameObject
+{
+public:
+    Player(GameObject* _parent);
+    ~Player();
+
+    void Initialize() override;
+
+    void Update() override;
+
+    void Draw() override;
+
+    void Release() override;
+
+private:
+    int hModel_;
+};
