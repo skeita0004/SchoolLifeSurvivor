6e2ae5c3f1e43a981746cbfa00959c20f20e9b91
啓大 齋藤 <240004@jc-21.jp>
2025-10-27 01:10:07 +0900

    BackGroundクラス追加

---
+++ BackGround.cpp
@@ -0,0 +1,36 @@
+﻿#include "BackGround.h"
+
+BackGround::BackGround(GameObject* _parent) :
+    GameObject(_parent, "BackGround"),
+    hImage_(-1)
+{
+}
+
+BackGround::~BackGround()
+{
+}
+
+void BackGround::Initialize()
+{
+    /* noimage.pngという、
+    バックグラウンドが設定されていないことを示す画像を用意する。*/
+    //hImage_ = Image::Load("noimage.png");
+}
+
+void BackGround::Update()
+{
+}
+
+void BackGround::Draw()
+{
+    Image::Draw(hImage_);
+}
+
+void BackGround::Release()
+{
+}
+
+void BackGround::SetBackGround(const std::string& _imagePath)
+{
+    hImage_ = Image::Load(_imagePath);
+}
---
+++ BackGround.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "GameObject.h"
+#include "Image.h"
+
+class BackGround : public GameObject
+{
+public:
+    BackGround(GameObject* _parent);
+    ~BackGround();
+
+    void Initialize() override;
+    void Update()     override;
+    void Draw()       override;
+    void Release()    override;
+
+    void SetBackGround(const std::string& _imagePath);
+private:
+    int hImage_;
+
+};
